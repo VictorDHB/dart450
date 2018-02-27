f9d10e2065fa7a46262759c4bcd5c5ae383b9db6
VICTOR LE <le_victo@cda021ev5709x.local>
2018-02-27 18:11:11 -0500

    update

--- exercises/exercise06/js/script.js
+++ exercises/exercise06/js/script.js
@@ -8,11 +8,18 @@ Pippin Barr
 
 $(document).ready(function() {
 
-  $("div").blast({ search: "subliminal" });
-  $("div").blast({ search: "evil" });
-  $("div").blast({ search: "ψ(｀∇´)ψ" })
-  $("div").blast({ search: "don't" })
-  $("div").blast({ search: "never" })
-  $("div").blast({ search: "not" })
-  $("div").blast({ search: "muahahahahaha!!!!" })
+  $("div").blast({ search: "subliminal" }).mouseover(reveal);
+  $("div").blast({ search: "evil" }).mouseover(reveal);
+  $("div").blast({ search: "ψ(｀∇´)ψ" }).mouseover(reveal);
+  $("div").blast({ search: "don't" }).mouseover(reveal);
+  $("div").blast({ search: "never" }).mouseover(reveal);
+  $("div").blast({ search: "not" }).mouseover(reveal);
+  $("div").blast({ search: "muahahahahaha!!!!" }).mouseover(reveal);
+
+function reveal () {
+  $(this).animate({
+      color: 'red',
+  });
+}
+
 });
