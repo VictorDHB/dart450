f093637f2dc1285acf0286032c489914a751a5e0
VICTOR LE <le_victo@cda021ev5709x.local>
2018-02-27 17:57:04 -0500

    Update

---
+++ exercises/exercise06/js/script.js
@@ -0,0 +1,18 @@
+/*
+
+Blast.js template
+Pippin Barr
+
+
+*/
+
+$(document).ready(function() {
+
+  $("div").blast({ search: "subliminal" });
+  $("div").blast({ search: "evil" });
+  $("div").blast({ search: "ψ(｀∇´)ψ" })
+  $("div").blast({ search: "don't" })
+  $("div").blast({ search: "never" })
+  $("div").blast({ search: "not" })
+  $("div").blast({ search: "muahahahahaha!!!!" })
+});
